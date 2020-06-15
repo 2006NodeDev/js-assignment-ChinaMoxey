f7a575d1d931cf9ace6f9c29b13a5ded342a27b1
ChinaMoxey <moxeychina@gmail.com>
2020-06-15 14:59:31 -0400

    defined function isPalindrome

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,17 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+  let re = '/[^A-Za-z0-9]/g';
+
+  someStr = someStr.toLowerCase().replace(re,'');
+
+  let len = someStr.length;
+    for(let i = 0; i < len/2; i++){
+        if(someStr[i]!==someStr[len-1-i]){
+            return false;
+        }
+
+    }
+    return true;
 }
+console.log(isPalindrome("madam"));

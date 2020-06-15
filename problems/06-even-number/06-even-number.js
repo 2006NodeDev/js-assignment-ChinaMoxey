f4a8fd485e1737276f9cb92a4549b009c94aa7c4
ChinaMoxey <moxeychina@gmail.com>
2020-06-15 11:58:44 -0400

    defined isEven function using bitwise

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -2,6 +2,14 @@
 Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
+
+//Using bitwise AND (&) operator to check if a number is even or odd
 function isEven(someNum) {
-  
+    if((someNum & 1) == 0){
+        return "This number is even";
+    }
+    else{
+        return "This number is odd";
+    }
 }
+console.log(isEven(5));

6a9ef6636423528f11169a31e2c67a66415de3dd
ChinaMoxey <moxeychina@gmail.com>
2020-06-15 14:02:12 -0400

    showed three different ways to create an object

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,28 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+//Creating an object using object literal syntax
+const person = {
+    name: 'China Moxey',
+    age: 22
+};
+console.log(person);
+//Creating an object using constructor function
+function personTwo(name,age){
+    this.name = name;
+    this.age = age;
+}
+const me = new personTwo('China Moxey',22);
+console.log(me);
+//Creating an object using ES6 classes
+class PersonThree{
+    constructor(fullname, realage){
+        this.name = fullname;
+        this.age = realage;
+    }
+}
+const objperson = new PersonThree('China Moxey', 22);
+
+console.log(objperson.name);
+console.log(objperson.age);

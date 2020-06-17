09aa7867cd5e2776e45a059b126b3f9ecc722e44
ChinaMoxey <moxeychina@gmail.com>
2020-06-17 15:00:29 -0400

    defined function for object properties

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,17 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
+
+const student = {
+    fname: 'China',
+    lname: 'Moxey',
+    age: 22, 
+}
 function objectProperties(someObj) {
+    
+    for(const key in someObj){
+     console.log(`${key} = ${student[key]}`);
+    }
 
 }
+console.log(objectProperties(student));

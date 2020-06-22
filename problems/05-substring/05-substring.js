ac30f7d6b9013930390d5e316a6a207a68239fb8
ChinaMoxey <moxeychina@gmail.com>
2020-06-22 17:40:25 -0400

    defined substring function

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,5 +4,18 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
-
+   
+   if(startIndex<0||someStr.length<startIndex){
+        throw("start input is incorrect");
+   }else if(endIndex<startIndex||someStr.length<=endIndex){
+        throw("end input is incorrect");
+   }else{
+       makeString="";
+       for(i = startIndex;i<endIndex;++i){
+            makeString+=someStr[i];
+       }
+       return makeString;
+   }
 }
+let myString = "Hello";
+console.log(substring(myString,0,3));
